e01df92e8bffd0c055924ad1cec3a28e19c13559
Rebecca Billings <rebeccabillings@host-5-172.mv.mozilla.com>
2013-05-22 14:37:58 -0700

    Updated locators, added fixes for edit profile

--- pages/profile.py
+++ pages/profile.py
@@ -9,15 +9,16 @@ from selenium.webdriver.common.by import By
 from pages.base import Base
 from pages.page import Page
 
+
 class Profile(Base):
 
-    _page_title = (By.CSS_SELECTOR, 'title:contains("Mozilla Reps - Profile*")')
-    _page_source = (By.CSS_SELECTOR, '#wrapper')
+    _page_title_locator = (By.CSS_SELECTOR, 'title:contains("Mozilla Reps - Profile*")')
+    _page_source_locator = (By.CSS_SELECTOR, '#wrapper')
     _edit_profile_locator = (By.CSS_SELECTOR, 'div.twelve a.small')
-    _email_locator = (By.CSS_SELECTOR, 'div.eleven p.profile-item')
     _firstname_locator = (By.CSS_SELECTOR, 'div.eleven h4')
     _lastname_locator = (By.CSS_SELECTOR, 'div.eleven h4:nth-of-type(2)')
-    _username_locator = (By.CSS_SELECTOR, 'div.eleven h5.grayed:nth-of-type(2)')
+    _name_fields_locator = (By.CSS_SELECTOR, 'div.eleven h')
+    _username_locator = (By.CSS_SELECTOR, 'div.eleven h5.grayed')
 
     def go_to_profile_page(self):
         self.selenium.get(self.base_url + '/me/')
@@ -25,16 +26,16 @@ class Profile(Base):
     def click_edit_profile_button(self):
         self.selenium.find_element(*self._edit_profile_locator).click()
 
-    def email_text(self):
-        return self.selenium.find_element(*self._email_locator).text
-
-    def username_text(self):
+    @property
+    def username(self):
         return self.selenium.find_element(*self._username_locator).text
 
-    def firstname_text(self):
+    @property
+    def firstname(self):
         return self.selenium.find_element(*self._firstname_locator).text
 
-    def lastname_text(self):
+    @property
+    def lastname(self):
         return self.selenium.find_element(*self._lastname_locator).text
 
     @property
@@ -49,6 +50,7 @@ class Profile(Base):
 
         _first_name_locator = (By.CSS_SELECTOR, 'div.eleven input.input-text:nth-of-type(3)')
         _last_name_locator = (By.CSS_SELECTOR, 'div.eleven input.input-text:nth-of-type(2)')
+        _profile_fields_locator =  (By.CSS_SELECTOR, 'div.eleven')
         _save_profile_locator = (By.CSS_SELECTOR, 'div.four button.small')
         _username_locator = (By.CSS_SELECTOR, 'div.eleven input.input-text')
 
@@ -59,6 +61,11 @@ class Profile(Base):
         def click_save_profile_button(self):
             self.selenium.find_element(*self._save_profile_locator).click()
             return Profile(self.testsetup)
+
+        @property
+        def profile_fields(self):
+            return [self.ProfileSection(self.testsetup, web_element)
+                for web_element in self.selenium.find_elements(*self._profile_fields_locator)]
 
         def set_first_name(self, firstname):
             element = self.selenium.find_element(*self._first_name_locator)
@@ -74,3 +81,27 @@ class Profile(Base):
             element = self.selenium.find_element(*self._username_locator)
             element.clear()
             element.send_keys(username)
+
+        def type_value(self, value):
+            self._root_element.find_element(*self._input_field_locator).send_keys(value)
+
+        class ProfileSection(Page):
+
+            _input_field_locator = (By.CSS_SELECTOR, ' input')
+
+            def __init__(self, testsetup, element):
+                Page.__init__(self, testsetup)
+                self._root_element = element
+
+            @property
+            def field_value(self):
+                try:
+                    return self._root_element.find_element(*self._input_field_locator).get_attribute('value')
+                except Exception.NoSuchAttributeException:
+                    return " "
+
+            def type_value(self, value):
+                self._root_element.find_element(*self._input_field_locator).send_keys(value)
+
+            def clear_field(self):
+                self._root_element.find_element(*self._input_field_locator).clear()
--- tests/test_profile_page.py
+++ tests/test_profile_page.py
@@ -24,9 +24,15 @@ class TestProfilePage:
         home.login()
         profile_page = Profile(mozwebqa)
         profile_page.go_to_profile_page()
-        Assert.true(profile_page.is_username_visible)
+
         profile_page.click_edit_profile_button()
-        edit_profile_page = EditProfile(mozwebqa)
+        edit_profile_page = Profile.EditProfile(mozwebqa)
+
+        # save initial values to restore them after the test is finished
+        fields_no = len(profile_page._name_fields_locator) - 1
+        initial_value = [None] * fields_no
+
+        # enter new profile values
         new_username = "TestUser%s" % random.randint(0, 100)
         new_lastname = "Lastname%s" % random.choice(string.ascii_letters)
         new_firstname = "First%s" % random.choice(string.ascii_letters)
@@ -34,9 +40,15 @@ class TestProfilePage:
         edit_profile_page.set_last_name(new_lastname)
         edit_profile_page.set_first_name(new_firstname)
         edit_profile_page.click_save_profile_button()
-        username = profile_page.username_text()
-        lastname = profile_page.lastname_text()
-        firstname = profile_page.firstname_text()
+        username = profile_page.username()
+        lastname = profile_page.lastname()
+        firstname = profile_page.firstname()
         Assert.equal(username, new_username)
         Assert.equal(lastname, new_lastname)
         Assert.equal(firstname, new_firstname)
+
+        # restore initial values
+        profile_page.click_edit_profile_button()
+        for i in range(0, fields_no):
+            edit_profile_page._profile_fields[i].type_value(initial_value[i])
+        edit_profile_page.click_save_profile_button()
