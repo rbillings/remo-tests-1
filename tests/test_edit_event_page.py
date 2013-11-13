2e8f0e07ba254004013fb356a7e1200b05ee3f86
rebeccabillings <rbillings@mozilla.com>
2013-11-13 10:54:48 -0800

    removed nondestructive mark

--- tests/test_edit_event_page.py
+++ tests/test_edit_event_page.py
@@ -17,7 +17,6 @@ from pages.home import Home
 class TestEditEventPage:
 
     @pytest.mark.credentials
-    @pytest.mark.nondestructive
     def test_edit_event(self, mozwebqa):
         home_page = Home(mozwebqa)
         home_page.login()
